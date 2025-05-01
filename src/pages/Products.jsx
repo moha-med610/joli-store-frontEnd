762fa812a684857ca629bc8e3a00cb004e2b0b92
mohamed khaled <mohakhaled324@gmail.com>
2025-05-01 09:38:12 +0300

    edit on categories

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,7 +4,6 @@ import Loading from "../components/Loading";
 import Slider from "../components/Products/Slider";
 import Search from "../components/Search";
 import Footer from "../components/Home/Footer"
-import Select from "../components/Products/Select";
 
 const Products = () => {
   const [data, setData] = useState([]);
@@ -12,7 +11,6 @@ const Products = () => {
   const [error, setError] = useState(null);
   const [category, setCategory] = useState("");
 
-  const categories = ["منتجات العنايه الشعر", "منتجات العنايه البشره", "منتجات العنايه بالجسم"]
 
   const url = category
         ? `https://e-commerce-joli-backend.onrender.com/api/products?category=${category}`
@@ -51,10 +49,16 @@ const Products = () => {
       <section id="products" className="text-center my-16 scroll-mt-24">
       <Search />
       <div className="flex justify-center items-center mt-4 mb-8">
-        <Select 
-          categories={categories}
-          setCategory={setCategory}
-        />
+        <select
+        className="bg-gray-50 border w-full lg:w-3/12 md:w-1/2 border-gray-300 text-gray-900 text-sm rounded-3xl focus:ring-blue-500 focus:border-blue-500 block p-3 pr-2 dark:bg-gray-100 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">اختار الفئة</option>
+          <option value="Hair">منتجات العنايه الشعر</option>
+          <option value="Skin">منتجات العنايه البشره</option>
+          <option value="Body">منتجات العنايه بالجسم</option>
+          
+        </select>
         </div>
         <h1 className="text-5xl font-bold text-pink-700 font-mono">المنتجات</h1>
       </section>
